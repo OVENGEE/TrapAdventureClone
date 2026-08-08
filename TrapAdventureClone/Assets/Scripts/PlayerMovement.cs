201def727568103cf558dd2be034ffbd6760a07b
OVENGEE <mpumelelokenneth911@gmail.com>
2026-08-08 20:11:35 +0200

    Adding Jumping and its gravity settings

--- TrapAdventureClone/Assets/Scripts/PlayerMovement.cs
+++ TrapAdventureClone/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    
-    float horizontalMovement;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    float horizontalMovement;
+    
+    [Header("Jumping")]
+    public float jumpPower = 10f;
+    
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
+
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);        
@@ -21,5 +26,37 @@ public class PlayerMovement : MonoBehaviour
     public void Move(InputAction.CallbackContext context)
     {
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(isGrounded())
+        {
+            if(context.performed)
+            {
+                //hold down jump button full height
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x , jumpPower);
+            }
+            else if(context.canceled)
+            {
+                //light tap of jump button = half the height
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            }
+        }
+    }
+
+    private bool isGrounded()
+    {
+        if(Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(groundCheckPos.position , groundCheckSize);
     }
 }
