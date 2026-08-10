5c9d0405f7457f666cc0496833de87f33d00665e
OVENGEE <mpumelelokenneth911@gmail.com>
2026-08-10 20:00:36 +0200

    Added player Health and Health UI
    player health but the player does not take damage yet

---
+++ TrapAdventureClone/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    public HealthUI healthUI;
+
+    private SpriteRenderer spriteRenderer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthUI.SetMaxHearts(maxHealth); 
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthUI.UpdateHearts(currentHealth);
+
+        //flash red
+        StartCoroutine(FlashRed());
+
+        if(currentHealth <= 0)
+        {
+            //player dead! -- call game over, animation, etc
+        }
+    }
+
+    private IEnumerator FlashRed()
+    {
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.2f);
+        spriteRenderer.color = Color.white;
+    }
+}
