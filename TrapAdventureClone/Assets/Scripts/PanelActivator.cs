a4bbf775b270c76f0891d7d7fa21cbb7bdd8231c
kopano Kgosimotswedi <kopanokopano174@gmail.com>
2026-08-10 13:46:45 +0200

    Win logic and UI

---
+++ TrapAdventureClone/Assets/Scripts/PanelActivator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject panelToEnable;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (panelToEnable != null)
+            {
+                panelToEnable.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToLevel1()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level1");
+    }
+}
