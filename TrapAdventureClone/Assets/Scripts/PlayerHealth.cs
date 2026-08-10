a8cfd96644a5bbcf526356959e109ccece4cbe4a
OVENGEE <mpumelelokenneth911@gmail.com>
2026-08-10 21:15:02 +0200

    Added the spike trap functionality
    ensured that the spike gives damage, if you get damaged you are reset to spawn  position, if you take damage and you move to the next scene your current health is carried on to the next scene

--- TrapAdventureClone/Assets/Scripts/PlayerHealth.cs
+++ TrapAdventureClone/Assets/Scripts/PlayerHealth.cs
@@ -9,13 +9,32 @@ public class PlayerHealth : MonoBehaviour
     public HealthUI healthUI;
 
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb;
+    private Vector3 spawnPosition;
+
+    private static int savedHealth;
+    private static bool hasSavedHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentHealth = maxHealth;
-        healthUI.SetMaxHearts(maxHealth); 
+        if (!hasSavedHealth)
+        {
+            currentHealth = maxHealth;
+            savedHealth = currentHealth;
+            hasSavedHealth = true;
+        }
+        else
+        {
+            currentHealth = savedHealth;
+        }
+
+        healthUI.SetMaxHearts(maxHealth);
+        healthUI.UpdateHearts(currentHealth);
 
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        spawnPosition = transform.position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,12 +43,14 @@ public class PlayerHealth : MonoBehaviour
         if(trap && trap.damage > 0)
         {
             TakeDamage(trap.damage);
+            ResetToSpawn();
         }
     }
 
     private void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        savedHealth = currentHealth;
         healthUI.UpdateHearts(currentHealth);
 
         //flash red
@@ -39,6 +60,17 @@ public class PlayerHealth : MonoBehaviour
         {
             //player dead! -- call game over, animation, etc
         }
+    }
+
+    private void ResetToSpawn()
+    {
+        if(rb)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+
+        transform.position = spawnPosition;
     }
 
     private IEnumerator FlashRed()
